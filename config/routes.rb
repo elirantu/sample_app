0429b2659a0a91b7f5b364d1a783fe9436563433
Eliran Turgeman <elirantu@gmail.com>
2011-11-10 15:05:00 +0200

    Done with sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 SampleApp::Application.routes.draw do
   resources :users
-  get "users/new"
-
-  match '/signup', :to => 'users#new'
-
-  match '/show', :to => 'users#show'
+  resources :sessions, :only => [:new, :create, :destroy]
   
+#  get "users/new"
+#  get "sessions/new"
+  
+  
+  match '/signup', :to => 'users#new'
+  match '/signin', :to => 'sessions#new'
+  match '/signout', :to => 'sessions#destroy'
+  match '/show', :to => 'users#show'
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
